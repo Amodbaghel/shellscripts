72db28a2503824f09a4148ce1a98e8753dfcf287
Amodbaghel <baghelamod012@.com>
2025-02-06 14:56:32 +0530

    this is for two

--- script1.sh
+++ script1.sh
@@ -3,7 +3,8 @@ echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "Hello batch-24, We are learning git"
 echo "Please complete your practicals on daily basis"
-echo "Hello guys"
+echo "good"
+
 
 
 
